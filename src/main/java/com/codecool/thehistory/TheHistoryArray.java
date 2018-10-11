4631f2c2cf4fa05697c3d9e9b2fde0978a3ffa6d
Noemi Szoke <szokenoemijob@gmail.com>
2018-10-11 10:02:24 +0200

    simple array is working

--- src/main/java/com/codecool/thehistory/TheHistoryArray.java
+++ src/main/java/com/codecool/thehistory/TheHistoryArray.java
@@ -12,32 +12,55 @@ public class TheHistoryArray implements TheHistory {
     @Override
     public void add(String text) {
         //TODO: check the TheHistory interface for more information
+        String[] toArray = text.split("\\s+");
+        wordsArray  =  Arrays.copyOf(toArray, toArray.length);
+
     }
 
     @Override
     public void removeWord(String wordToBeRemoved) {
         //TODO: check the TheHistory interface for more information
+        StringBuilder wordsWithout = new StringBuilder();
+        for (String word: wordsArray) {
+            if (!word.equals(wordToBeRemoved)){
+                wordsWithout.append(word).append(" ");
+            }
+        }
+        String wordsWo = wordsWithout.toString();
+        add(wordsWo);
     }
 
     @Override
     public int size() {
         //TODO: check the TheHistory interface for more information
-        return 0;
+        return wordsArray.length;
     }
 
     @Override
     public void clear() {
         //TODO: check the TheHistory interface for more information
+        wordsArray = new String[0];
     }
 
     @Override
     public void replaceOneWord(String from, String to) {
         //TODO: check the TheHistory interface for more information
+
+        for (int i = 0; i < wordsArray.length; i++){
+            wordsArray[i]= wordsArray[i].replaceAll(from, to);
+        }
     }
 
     @Override
     public void replaceMoreWords(String[] fromWords, String[] toWords) {
         //TODO: check the TheHistory interface for more information
+
+        String fromW = Arrays.toString(fromWords).replace(",","").replace("[", "").replace("]", "").trim();
+        String toW = Arrays.toString(toWords).replace(",","").replace("[", "").replace("]", "").trim();
+        String originalString = Arrays.toString(wordsArray).replace(",","").replace("[", "").replace("]", "").trim();
+        originalString = originalString.replaceAll(fromW,toW);
+        add(originalString);
+
     }
 
     @Override
